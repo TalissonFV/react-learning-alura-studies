7384553c3b2915491e9deaa84ec127ef1e74ad12
talissonfv <talissonfvilela@gmail.com>
2024-12-03 22:58:17 -0300

    refatorado props do formulario

--- src/components/Formulario/index.tsx
+++ src/components/Formulario/index.tsx
@@ -4,8 +4,12 @@ import style from './Formulario.module.scss'
 import { ITarefa } from '../../types/tarefas';
 import { v4 as uuidv4 } from 'uuid'
 
+interface IProps {
+    setTarefas: React.Dispatch<React.SetStateAction<ITarefa[]>>
+}
 
-export default function Formulario({setTarefas}: {setTarefas: React.Dispatch<React.SetStateAction<ITarefa[]>>}):React.JSX.Element {
+
+export default function Formulario({setTarefas}: IProps):React.JSX.Element {
 
     const [nome, setNome] = useState("");
     const [tempo, setTempo] = useState("00:00");
@@ -21,11 +25,11 @@ export default function Formulario({setTarefas}: {setTarefas: React.Dispatch<Rea
         <form className={style.novaTarefa} onSubmit={salvarTarefa}>
             <div className={style.inputContainer}>
                 <label htmlFor='nome'>Adicione um novo estudo</label>
-                <input type='text' value={nome} onChange={(evento) => {setNome(evento.target.value)}} name='nome' id='nome' placeholder='O que você quer estudar?' required></input>
+                <input type='text' value={nome} onChange={(evento) => setNome(evento.target.value)} name='nome' id='nome' placeholder='O que você quer estudar?' required></input>
             </div>
             <div className={style.inputContainer}>
                 <label htmlFor='tempo'>Tempo</label>
-                <input type='time' onChange={(evento) => {setTempo(evento.target.value)}} value={tempo} step='1' id='tempo' name='tempo' min='00:00:00' max='01:30:00' required></input>
+                <input type='time' onChange={(evento) => setTempo(evento.target.value)} value={tempo} step='1' id='tempo' name='tempo' min='00:00:00' max='01:30:00' required></input>
             </div>
             <Botao type="submit"> Adicionar </Botao>
         </form>
